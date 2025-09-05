1434d5c6e625d9ce47942e7b49154014fddcaba8
ctpride07 <ctpride07@gmail.com>
2025-09-05 11:15:38 -0400

    Created, tested, and finalized a code to do the defined function.

--- Chapter2Exercise7.cpp
+++ Chapter2Exercise7.cpp
@@ -3,7 +3,7 @@
 *Programmer: Carter Pride
 *Date: 09/25
 */
-//Write a C++ program to determine the amount that the current ocean level will rise in 5, 7, and 10 years. The level rises 1.5 millimeters per year.
+//Function: Write a C++ program to determine the amount that the current ocean level will rise in 5, 7, and 10 years. The level rises 1.5 millimeters per year.
 
 #include <iostream>
 using namespace std;
@@ -11,15 +11,20 @@ int main()
 {
 	double oceanlevel;
 	double annualrateofrise = 1.5;
-	double levelafter5years = oceanlevel + annualrateofrise * 5;
-	double levelafter7years = oceanlevel + annualrateofrise * 7;
-	double levelafter10years = oceanlevel + annualrateofrise * 10;
+	double levelafter5years;
+	double levelafter7years;
+	double levelafter10years;
 
-	cout << "Please enter the current ocean level in millimeters" << endl;
+
+	cout << "Please enter the current ocean level in millimeters " << endl;
 	cin >> oceanlevel;
-	cout << "The ocean level after 5 years will be" << levelafter5years << endl;
-	cout << "The ocean level after 7 years will be" << levelafter7years << endl;
-	cout << "The ocean level after 10 years will be" << levelafter10years << endl;
+	levelafter5years = oceanlevel + annualrateofrise * 5;
+	levelafter7years = oceanlevel + annualrateofrise * 7;
+	levelafter10years = oceanlevel + annualrateofrise * 10;
+
+	cout << "The ocean level after 5 years will be " << levelafter5years << endl;
+	cout << "The ocean level after 7 years will be " << levelafter7years << endl;
+	cout << "The ocean level after 10 years will be " << levelafter10years << endl;
 	return 1;
 }
 
